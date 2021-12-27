7e1f9e438ad4f158731c5e5325e424a7a9700dca
Manav Misra <manavm@visionwebsoft.com>
2021-12-26 18:51:14 -0600

    :seedling: usersRouter

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import usersRouter from './users';
 
 const router = Router();
 
 router.get('/', (_, res) => {
   res.send('Hello World!');
 });
-
-router.use('/users', usersRouter);
 
 export default router;
