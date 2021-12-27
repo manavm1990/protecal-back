0ed72b80e1342d96b4d89c32e5d483e82b852fe9
Manav Misra <manavm@visionwebsoft.com>
2021-12-26 18:47:52 -0600

    :seedling: Api router

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import config from './config';
+import router from './routes';
 
 const app = express();
 
 app.get('/', (_, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api', router);
 
 app.listen(config.port, () => {
   console.info('Server is listening on port', config.port);
