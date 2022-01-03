76607117d23feaf76c95790d0457816a8dbe64ac
Manav Misra <manavm@visionwebsoft.com>
2022-01-02 19:09:18 -0600

    Scope db config stuff into 'db'
    Includes `dataSource` and `database`

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 type configType = {
-  apiKey: string;
+  db: {
+    apiKey: string;
+    apiUrl: string;
+    dataSource: 'Cluster0';
+    database: 'protecal';
+  };
   port: string | number;
 };
 
 const config: configType = {
-  // ⚠️ Must guarantee this value is set in .env file
-  apiKey: process.env.API_KEY!,
-
+  db: {
+    apiKey: process.env.API_KEY!,
+    apiUrl: process.env.API_URL!,
+    dataSource: 'Cluster0',
+    database: 'protecal',
+  },
   // If coming from .env file, this value is a string
   port: process.env.PORT || 3000,
 };
