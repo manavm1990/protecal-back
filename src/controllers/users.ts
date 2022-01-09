18cd970978aa7475132698dcabe6c07740dbf8d9
Manav Misra <manavm@visionwebsoft.com>
2022-01-08 20:39:08 -0600

    :recycle: `getExistingUser` fxn

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,10 @@ import got from 'got';
 import config from '../config';
 import User from '../models/User';
 
+type UserDocument = {
+  document: User | null;
+};
+
 const headers = { 'api-key': config.db.apiKey };
 const payloadBase = {
   collection: 'users',
@@ -9,19 +13,22 @@ const payloadBase = {
   database: config.db.database,
 };
 
+const getExistingUser = (username: string): Promise<UserDocument> =>
+  got
+    .post(`${config.db.apiUrl}/findOne`, {
+      headers,
+      json: {
+        ...payloadBase,
+        filter: {
+          username,
+        },
+      },
+    })
+    .json();
+
 export default {
   async create(newUser: User) {
-    const { document: existingUser } = await got
-      .post(`${config.db.apiUrl}/findOne`, {
-        headers,
-        json: {
-          ...payloadBase,
-          filter: {
-            username: newUser.username,
-          },
-        },
-      })
-      .json();
+    const { document: existingUser } = await getExistingUser(newUser.username);
 
     if (existingUser) {
       throw new Error('User already exists');
